70f72329fe8505e9f107dc166b94696c294324e5
havishwa <havishwa@cisco.com>
2016-06-07 10:49:40 +0530

    run on all interfaces

--- src/main/resources/startup.sh
+++ src/main/resources/startup.sh
@@ -1,1 +1,1 @@
-java -cp ".:modbusSimulator-0.0.1-SNAPSHOT.jar:third-party/*" com.cisco.iox.simulators.modbus.ModbusSlaveSimulator localhost 502
+java -cp ".:modbusSimulator-0.0.1-SNAPSHOT.jar:third-party/*" com.cisco.iox.simulators.modbus.ModbusSlaveSimulator 0.0.0.0 502
